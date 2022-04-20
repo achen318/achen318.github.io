afcd52de35cb03d90a6cad1a7dd026e6e562a8a9
achen318 <50970130+achen318@users.noreply.github.com>
2022-04-19 22:57:46 -0400

    Add footer component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import Footer from '../components/Footer';
 import Meta from '../components/Meta';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      <Meta title="Home Page"/>
+      <Meta title="Home Page" />
 
       <div className={styles.container}>
         <main className={styles.main}>
@@ -49,23 +49,7 @@ export default function Home() {
           </div>
         </main>
 
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className={styles.logo}>
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={72}
-                height={16}
-              />
-            </span>
-          </a>
-        </footer>
+        <Footer />
       </div>
     </>
   );
