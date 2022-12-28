a4cb927cee281689f86df1010f9bcad064b81bf2
Anthony Chen <50970130+achen318@users.noreply.github.com>
2022-12-27 22:55:31 -0500

    skillz 100

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
-export interface Social {
+export interface SocialType {
   name: string;
   url: string;
   icon: string;
+}
+
+export interface SkillType {
+  name: string;
+  icon: string;
+  year: number; // initial year of learning
 }
